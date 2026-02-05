1047602887071c5196ebc6b9a526131466475c10
Lier Rodriguez Rey <lier.rodriguez04rsomo.eus>
2026-02-05 13:19:41 +0100

    Proyecto CalculadoraGIT18 clase Calculadora y método sumar y restar y multiplicar y dividir division controlada

--- CalculadoraGIT18/src/calculadora18/Calculadora18.java
+++ CalculadoraGIT18/src/calculadora18/Calculadora18.java
@@ -10,8 +10,13 @@ public class Calculadora18 {
 	public int multiplicar(int a, int b) { 
 		 return a * b; 
 		} 
-		public double dividir(int a, int b) { 
+	public double dividir(int a, int b) { 
 		 return (double) a / b; 
+		} 
+	public double dividir(int a, int b) { 
+			if (b == 0) { 
+			throw new IllegalArgumentException("No se puede dividir entre cero");  } 
+			return (double) a / b; 
 		} 
 
 }
