3da95ac6923f94810b1bcb6681a473656ad5dbe8
Lier Rodriguez Rey <lier.rodriguez04rsomo.eus>
2026-02-09 13:38:55 +0100

    Proyecto CalculadoraGIT18 clase Calculadora exponente

---
+++ Calculadora18.java
@@ -0,0 +1,29 @@
+[33mcommit 1047602887071c5196ebc6b9a526131466475c10[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmaster[m[33m, [m[1;31morigin/master[m[33m)[m
+Author: Lier Rodriguez Rey <lier.rodriguez04rsomo.eus>
+Date:   Thu Feb 5 13:19:41 2026 +0100
+
+    Proyecto CalculadoraGIT18 clase Calculadora y método sumar y restar y multiplicar y dividir division controlada
+
+[33mcommit 6e553428ff37614775ba12e067b0f4820292c76c[m
+Author: Lier Rodriguez Rey <lier.rodriguez04rsomo.eus>
+Date:   Thu Feb 5 13:15:56 2026 +0100
+
+    Proyecto CalculadoraGIT18 clase Calculadora y método sumar y restar y multiplicar y dividir
+
+[33mcommit 4b49a2712fd79117c532736e3c5c1fc31a4f4908[m
+Author: Lier Rodriguez Rey <lier.rodriguez04rsomo.eus>
+Date:   Thu Feb 5 13:14:10 2026 +0100
+
+    Proyecto CalculadoraGIT18 clase Calculadora y método sumar y restar
+
+[33mcommit dada92c9f13575a76923d707712a2c765e530cd2[m
+Author: Lier Rodriguez Rey <lier.rodriguez04rsomo.eus>
+Date:   Thu Feb 5 12:48:14 2026 +0100
+
+    Proyecto CalculadoraGIT18 clase Calculadora y método sumar
+
+[33mcommit 0ee0509a9e542c4ba8affadef00f860ab1f6c308[m
+Author: Lier Rodriguez Rey <lier.rodriguez04rsomo.eus>
+Date:   Mon Feb 2 13:51:33 2026 +0100
+
+    Mensaje claro de lo que hiciste
--- CalculadoraGIT18/src/calculadora18/Calculadora18.java
+++ CalculadoraGIT18/src/calculadora18/Calculadora18.java
@@ -4,5 +4,29 @@ public class Calculadora18 {
 	public int sumar(int a, int b) {
 		 return a + b;
 		 }
+	public int restar(int a, int b) {
+		 return a - b;
+		 }
+	public int multiplicar(int a, int b) { 
+		 return a * b; 
+		} 
+	public double dividir(int a, int b) { 
+		 return (double) a / b; 
+		} 
+	public double dividir(int a, int b) { 
+			if (b == 0) { 
+			throw new IllegalArgumentException("No se puede dividir entre cero");  } 
+			return (double) a / b; 
+		} 
+	public int potencia(int base, int exponente) { 
+		 int resultado = 1; 
+		 for (int i = 0; i < exponente; i++) { 
+		 resultado *= base; 
+		 } 
+		 return resultado; 
+		} 
+		public int modulo(int a, int b) { 
+		 return a % b; 
+		} 
 
 }
