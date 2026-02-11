fd370c58d8cd1503d5f4fda620c6c5fb52d06f88
Lier Rodriguez Rey <lier.rodriguez04rsomo.eus>
2026-02-11 08:31:23 +0100

    Merge de feature-avanzadas y resolución de conflicto en dividir

--- CalculadoraGIT18/src/calculadora18/Calculadora18.java
+++ CalculadoraGIT18/src/calculadora18/Calculadora18.java
@@ -4,5 +4,29 @@ public class Calculadora18 {
 	public int sumar(int a, int b) {
 		 return a + b;
 		 }
+	public int restar(int a, int b) {
+		 return a - b;
+		 }
+	public int multiplicar(int a, int b) { 
+		 return a * b; 
+		} 
+	public double dividir(int a, int b) { 
+		 return (double) a / b; 
+		} 
+	public double dividir(int a, int b) { 
+			if (b == 0) { 
+			throw new IllegalArgumentException("No se puede dividir entre cero");  } 
+			return (double) a / b; 
+		} 
+
+	public double dividir(int a, int b) { 
+		 if (b == 0) { 
+		 System.out.println("Error: división entre cero"); 
+		 return 0; 
+		 } 
+		 return (double) a / b; 
+		} 
+
+
 
 }
